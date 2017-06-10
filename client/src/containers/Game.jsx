ba37a8c63cb51d3ac254244dd7c340c0e62e42ba
Rhys Brame <rhysbrame@gmail.com>
2017-06-10 12:22:07 +0100

    set up project and tested run with title running on browser

---
+++ client/src/containers/Game.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div>
+        Game of Guess Who?
+      </div>
+      )
+  }
+}
+
+export default Game;
